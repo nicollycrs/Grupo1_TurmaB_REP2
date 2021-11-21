03215a9bf154219ad0bd3c7eee43d15214a27649
NICOLLY CANDIDA RODRIGUES DE SOUZA <nicolly.crsouza@senacsp.edu.br>
2021-11-21 15:20:01 -0300

    primeiro commit

---
+++ Pandemus/src/main/java/com/senac/tads/turmanb/pi1/grupo1/pandemus/Pandemus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.tads.turmanb.pi1.grupo1.pandemus;
+
+/**
+ *
+ * @author nicolly.crsouza
+ */
+public class Pandemus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
